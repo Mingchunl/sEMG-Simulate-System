8984ae3762553cd01e0d78134c56f22769ec8c50
Mingchunl <302852839@qq.com>
2024-06-06 22:04:32 -0400

    Delete sEMGSimulateSystem directory

--- sEMGSimulateSystem/circle.m
+++
@@ -1,9 +0,0 @@
-function [cx cy] = circle(x,y,r)
-%生成肌肉MU分布的圆
-%   此处显示详细说明
-% 程序如下：
-t=0:pi/100:2*pi;
-cy=r*sin(t)+y;
-cx=r*cos(t)+x;
-end
-
--- sEMGSimulateSystem/distance.m
+++
@@ -1,3 +0,0 @@
-function r=distance(x, y, xc, yc) %(x,y)距离（xc,yc)
-r = sqrt((x - xc)^2 + (y - yc)^2);
-end
